7bf806893c6ca43e99c06269454c33afcce7897a
John Hashem <jphyqr@gmail.com>
2018-04-20 00:33:20 -0600

    Remove the now ignored directory node_modules

--- dist/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import CampaignFactory from "./build/CampaignFactory.json";
-
-const instance = new web3.eth.Contract(
-	JSON.parse(CampaignFactory.interface),
-	"0x5573FbD93C97973EF860fFbBf18A4381d3f1C8d5"
-);
-
-export default instance;
--- dist/show.js
+++
@@ -1,6 +0,0 @@
-//newcampaign.js;
-import React from "react";
-
-export default () => {
-	return <h1>welcome to show page</h1>;
-};
